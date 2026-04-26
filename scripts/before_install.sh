20e155abb2fa0b988d16fa25d51159bb89fc0950
rima <armande.kanm7@gmail.com>
2026-04-26 11:54:59 -0400

    giving x permission

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+export HOME=/home/ec2-user
+
+#download node and npm only if not already installed
+if [ ! -f "$HOME/.nvm/nvm.sh" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+fi
+
+. $HOME/.nvm/nvm.sh
+
+if ! command -v node &> /dev/null; then
+  nvm install --lts
+fi
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/techflare-backend"
